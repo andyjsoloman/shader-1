8ff1483d7067b8c7e3870219a4d5c5f289b7b609
Andy Soloman <andyjsoloman@gmail.com>
2024-03-15 20:03:13 -0700

    drei & orbitcontrols

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Canvas } from "@react-three/fiber";
+
 import "./App.css";
 import Experience from "./Experience";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
   return (
     <>
       <Canvas>
+        <OrbitControls />
         <Experience />
       </Canvas>
     </>
