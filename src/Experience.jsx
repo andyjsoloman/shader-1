064aad7dc6dc7c3b91e73288bc47f37e2bdd1327
Andy Soloman <andyjsoloman@gmail.com>
2024-03-18 21:34:11 -0700

    add vite-glsl-plugin not reloading on glsl save

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,10 +1,16 @@
-export default function Experience() {
+import { useRef } from "react";
+import vertexShader from "./shaders/test/vertex.glsl";
+import fragmentShader from "./shaders/test/fragment.glsl";
+export default function ShaderPlane() {
+  const mesh = useRef();
+
   return (
-    <>
-      <mesh>
-        <torusKnotGeometry />
-        <meshNormalMaterial />
-      </mesh>
-    </>
+    <mesh ref={mesh}>
+      <planeGeometry args={[5, 5, 32, 32]} />
+      <shaderMaterial
+        fragmentShader={fragmentShader}
+        vertexShader={vertexShader}
+      />
+    </mesh>
   );
 }
