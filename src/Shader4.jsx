7d412c851a88eaa88ab243578039496ddee87a93
Andy Soloman <andyjsoloman@gmail.com>
2024-11-30 12:50:03 -0800

    add gui to shader 4

--- src/Shader4.jsx
+++ src/Shader4.jsx
@@ -1,11 +1,13 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
-import { useRef, useMemo } from "react";
+import { useRef, useMemo, useEffect } from "react";
 import * as THREE from "three";
 import vertexShader from "./shaders/shader4/vertex-s4.glsl";
 import fragmentShader from "./shaders/shader4/fragment-s4.glsl";
 import { useFrame } from "@react-three/fiber";
+import { GUI } from "lil-gui";
 
-export default function Shader3() {
+export default function Shader4() {
   const mesh = useRef();
 
   const uniforms = useMemo(
@@ -22,6 +24,16 @@ export default function Shader3() {
     const { clock } = state;
     mesh.current.material.uniforms.u_time.value = clock.getElapsedTime();
   });
+
+  useEffect(() => {
+    const gui = new GUI();
+    gui.add(mesh.current.rotation, "x", 0, Math.PI * 2);
+    gui.add(mesh.current.rotation, "y", 0, Math.PI * 2);
+    gui.add(mesh.current.rotation, "z", 0, Math.PI * 2);
+    return () => {
+      gui.destroy();
+    };
+  }, []);
 
   return (
     <mesh ref={mesh}>
