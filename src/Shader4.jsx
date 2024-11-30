4d7ebf2cb1c45189851b312efbabea4a3d7accb4
Andy Soloman <andyjsoloman@gmail.com>
2024-11-30 15:29:40 -0800

    finish shader 4 sea

--- src/Shader4.jsx
+++ src/Shader4.jsx
@@ -9,6 +9,10 @@ import { GUI } from "lil-gui";
 
 export default function Shader4() {
   const mesh = useRef();
+  const debugObject = {};
+
+  debugObject.depthColor = "#186691";
+  debugObject.surfaceColor = "#9BD8FF";
 
   const uniforms = useMemo(
     () => ({
@@ -16,6 +20,17 @@ export default function Shader4() {
         value: 0.0,
       },
       u_frequency: { value: new THREE.Vector2(10, 5) },
+      u_bigWavesElevation: { value: 0.2 },
+      u_bigWavesFrequency: { value: new THREE.Vector2(1, 2) },
+      u_bigWavesSpeed: { value: 0.75 },
+      u_depthColor: { value: new THREE.Color(debugObject.depthColor) },
+      u_surfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
+      u_colorOffset: { value: 0.175 },
+      u_colorMultiplier: { value: 5 },
+      u_smallWavesElevation: { value: 0.15 },
+      u_smallWavesFrequency: { value: 1.5 },
+      u_smallWavesSpeed: { value: 0.2 },
+      u_smallWavesIterations: { value: 4 },
     }),
     []
   );
@@ -26,18 +41,81 @@ export default function Shader4() {
   });
 
   useEffect(() => {
-    const gui = new GUI();
+    const gui = new GUI({ width: 400 });
     gui.add(mesh.current.rotation, "x", 0, Math.PI * 2);
     gui.add(mesh.current.rotation, "y", 0, Math.PI * 2);
     gui.add(mesh.current.rotation, "z", 0, Math.PI * 2);
+    gui
+      .add(uniforms.u_bigWavesElevation, "value")
+      .min(0)
+      .max(1)
+      .name("u_bigWavesElevation");
+    gui
+      .add(uniforms.u_bigWavesFrequency.value, "x")
+      .min(0)
+      .max(10)
+      .name("u_bigWavesFrequencyX");
+    gui
+      .add(uniforms.u_bigWavesFrequency.value, "y")
+      .min(0)
+      .max(10)
+      .name("u_bigWavesFrequencyY");
+    gui
+      .add(uniforms.u_bigWavesSpeed, "value")
+      .min(0)
+      .max(4)
+      .name("u_bigWavesSpeed");
+    gui
+      .add(uniforms.u_smallWavesElevation, "value")
+      .min(0)
+      .max(1)
+      .name("u_smallWavesElevation");
+    gui
+      .add(uniforms.u_smallWavesFrequency, "value")
+      .min(0)
+      .max(10)
+      .name("u_smallWavesFrequency");
+    gui
+      .add(uniforms.u_smallWavesSpeed, "value")
+      .min(0)
+      .max(4)
+      .name("u_smallWavesSpeed");
+    gui
+      .add(uniforms.u_smallWavesIterations, "value")
+      .min(0)
+      .max(5)
+      .step(1)
+      .name("u_smallWavesIterations");
+    gui
+      .addColor(debugObject, "depthColor")
+      .name("depthColor")
+      .onChange(() => {
+        uniforms.u_depthColor.value.set(debugObject.depthColor);
+      });
+    gui
+      .addColor(debugObject, "surfaceColor")
+      .name("surfaceColor")
+      .onChange(() => {
+        uniforms.u_depthColor.value.set(debugObject.surfaceColor);
+      });
+    gui
+      .add(uniforms.u_colorOffset, "value")
+      .min(0)
+      .max(1)
+      .name("u_colorOffset");
+    gui
+      .add(uniforms.u_colorMultiplier, "value")
+      .min(0)
+      .max(10)
+      .name("u_colorMultiplier");
     return () => {
       gui.destroy();
     };
   }, []);
 
   return (
-    <mesh ref={mesh}>
-      <planeGeometry args={[5, 5, 128, 128]} />
+    <mesh ref={mesh} rotation={[Math.PI / 1.6, 0, 0]}>
+      <planeGeometry args={[5, 5, 512, 512]} />
       <shaderMaterial
         fragmentShader={fragmentShader}
         vertexShader={vertexShader}
--- src/shaders/shader4/fragment-s4.glsl
+++ src/shaders/shader4/fragment-s4.glsl
@@ -1,14 +1,20 @@
 precision mediump float;
 
+uniform vec3 u_depthColor;
+uniform vec3 u_surfaceColor;
+uniform float u_colorOffset;
+uniform float u_colorMultiplier;
+
 varying vec2 vUv;
+varying float vElevation;
 
 
 
 void main() {
+float mixStrength = (vElevation + u_colorOffset) * u_colorMultiplier;
+vec3 color = mix(u_depthColor, u_surfaceColor, mixStrength);
 
-
-
-  gl_FragColor = vec4(1.0,0.5,0.5,1.0);
+  gl_FragColor = vec4(color,1.0);
 
 
 }
